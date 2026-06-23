ac6e953204528cdb0e0e79eb39c64342cd5c4499
K-Sz Daniel <kalmardaniel90@gmail.com>
2026-06-23 12:20:23 +0200

    init project, add docker-compose.yaml for dependencies and services

--- src/test/java/miltdev/com/rabbitmqdemo/RabbitMqDemoApplicationTests.java
+++ src/test/java/miltdev/com/rabbitmqdemo/RabbitMqDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RabbitMqDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
